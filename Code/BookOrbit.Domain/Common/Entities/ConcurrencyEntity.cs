71b4f4528ce743906b9613f0f43988468941fcc3
Eyad-Dawood <eyad.dawood.dev@gmail.com>
2026-05-12 14:58:55 +0300

    Add ConcurrencyEntity and EF config
    Introduce an abstract ConcurrencyEntity (inherits AuditableEntity) adding a RowVersion byte[] for optimistic concurrency along with constructors. Add an EF Core configuration extension ConfigureAuditable<T> (where T : ConcurrencyEntity) that configures the RowVersion property as a row version / concurrency token (IsRowVersion, IsConcurrencyToken). This centralizes concurrency configuration for entities that require optimistic concurrency control.

---
+++ Code/BookOrbit.Domain/Common/Entities/ConcurrencyEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace BookOrbit.Domain.Common.Entities;
+public abstract class ConcurrencyEntity : AuditableEntity
+{
+    public byte[] RowVersion { get; set; } = null!;
+    protected ConcurrencyEntity()
+    {
+    }
+
+    protected ConcurrencyEntity(Guid id) : base(id)
+    {
+    }
+}
---
+++ Code/BookOrbit.Infrastructure/Data/Configurations/ConcurrencyEntityConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace BookOrbit.Infrastructure.Data.Configurations;
+public static class ConcurrencyEntityConfiguration
+{
+    public static void ConfigureAuditable<T>(this EntityTypeBuilder<T> builder)
+            where T : ConcurrencyEntity
+    {
+        builder.Property(e => e.RowVersion)
+            .IsRowVersion()
+            .IsConcurrencyToken();
+    }
+}
