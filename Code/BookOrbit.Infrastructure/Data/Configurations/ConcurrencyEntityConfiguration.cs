01c384cf2409977c4e692f0cfad2f1eea57f94a6
Eyad-Dawood <eyad.dawood.dev@gmail.com>
2026-05-15 12:16:45 +0300

    Update ConcurrencyEntityConfiguration.cs

--- Code/BookOrbit.Infrastructure/Data/Configurations/ConcurrencyEntityConfiguration.cs
+++ Code/BookOrbit.Infrastructure/Data/Configurations/ConcurrencyEntityConfiguration.cs
@@ -1,7 +1,7 @@
 ﻿namespace BookOrbit.Infrastructure.Data.Configurations;
 public static class ConcurrencyEntityConfiguration
 {
-    public static void ConfigureAuditable<T>(this EntityTypeBuilder<T> builder)
+    public static void ConfigureConcurrency<T>(this EntityTypeBuilder<T> builder)
             where T : ConcurrencyEntity
     {
         builder.Property(e => e.RowVersion)
