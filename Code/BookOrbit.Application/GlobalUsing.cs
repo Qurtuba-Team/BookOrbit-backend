feb8503dfff44893176cec793739f48ea651994e
Eyad-Dawood <eyad.dawood.dev@gmail.com>
2026-05-15 21:04:41 +0300

    Update GlobalUsing.cs

--- Code/BookOrbit.Application/GlobalUsing.cs
+++ Code/BookOrbit.Application/GlobalUsing.cs
@@ -85,3 +85,4 @@ global using System.Linq.Expressions;
 global using System.Reflection;
 global using System.Security.Claims;
 global using System.Text.Json.Serialization;
+global using BookOrbit.Application.Common.Interfaces.GetAllQueryHandlers;
