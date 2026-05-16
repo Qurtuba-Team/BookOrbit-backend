3325733211e2239638a14751e65d62d15b5b59b2
Eyad-Dawood <eyad.dawood.dev@gmail.com>
2026-05-16 17:06:12 +0300

    Create RefreshTokenQueryValidator.cs

---
+++ Code/BookOrbit.Application/Features/Identity/Queries/RefreshTokens/RefreshTokenQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace BookOrbit.Application.Features.Identity.Queries.RefreshTokens;
+public class RefreshTokenQueryValidator : AbstractValidator<RefreshTokenQuery>
+{
+    public RefreshTokenQueryValidator()
+    {
+        RuleFor(x => x.RefreshToken)
+           .Cascade(CascadeMode.Stop)
+           .NotEmpty()
+           .WithMessage(RefreshTokenErrors.TokenRequired.Description);
+    }
+}
